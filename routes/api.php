4e01254e4351ba8687ab0a064ba6e7684fb75862
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2024-02-02 21:30:52 +0600

    code samurai

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('users/{id}', function ($id) {
+    return response()->json(['id' => $id])->setStatusCode(201);
+
+});
